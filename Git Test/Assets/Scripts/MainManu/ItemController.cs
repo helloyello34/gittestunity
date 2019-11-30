cb36aa861b64ff825e4fddbe4c753ed2211bf5c1
Kristinn <kristinn34@gmail.com>
2019-11-30 11:08:31 +0000

    ASf

---
+++ Git Test/Assets/Scripts/MainManu/ItemController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemController : MonoBehaviour
+{
+    public TMPro.TextMeshProUGUI startLabel;
+    public TMPro.TextMeshProUGUI quitLabel;
+    uint selected;
+    // Start is called before the first frame update
+    void Start()
+    {
+        selected = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("up"))
+        {
+            selected += 1;
+            Select();
+        }
+        if (Input.GetKeyDown("down"))
+        {
+            selected -= 1;
+            Select();
+        }
+        if (Input.GetKeyDown("return") || Input.GetKeyDown("space"))
+        {
+            Debug.Log(selected % 2);
+        }
+    }
+
+    void Select()
+    {
+        uint n = selected % 2;
+        if (n == 0)
+        {
+            startLabel.transform.position += new Vector3(10, 0, 0);
+            quitLabel.transform.position += new Vector3(-10, 0, 0);
+        }
+        if(n == 1)
+        {
+            startLabel.transform.position += new Vector3(-10, 0, 0);
+            quitLabel.transform.position += new Vector3(10, 0, 0);
+        }
+    
+    }
+}
---
+++ Git Test/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    Vector3 walkDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        walkDirection = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w"))
+            walkDirection.y += 1;
+        if (Input.GetKey("s"))
+            walkDirection.y -= 1;
+        if(Input.GetKey("a"))
+            walkDirection.x -= 1;
+        if (Input.GetKey("d"))
+            walkDirection.x += 1;
+
+        walkDirection *= (Time.deltaTime * speed);
+
+        Walk();
+    }
+
+    void Walk()
+    {
+        transform.position += walkDirection;
+    }
+}
